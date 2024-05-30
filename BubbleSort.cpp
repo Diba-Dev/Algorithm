c9978b3ff65fd577b22ca17d3221fe1f36c13cd8
Diba-Dev <dibadev909090@gmail.com>
2024-05-31 00:51:09 +0600

    box is set

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -3,21 +3,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubbleShort(int array[], int size){
-    
+void display(int array[], int size)
+{
+    int boxWidth = 4;
+
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "+----";
+    }
+    cout << "+" << endl;
+
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "| " << setw(boxWidth - 2) << array[i] << " ";
+    }
+    cout << "|" << endl;
+
+    // Print the bottom border
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "+----"; // Adjust the number of '-' according to boxWidth
+    }
+    cout << "+" << endl;
+}
+
+void bubbleShort(int array[], int size)
+{
+
     for (int i = 0; i < size; i++)
     {
+        cout << "pass[" << i << "] :" << endl;
+        display(array, size);
         int flag = 0;
-        for (int j = 0; j<size - i - 1; j++)
+        for (int j = 0; j < size - i - 1; j++)
         {
-            if (array[j+1]<array[j])
+            if (array[j + 1] < array[j])
             {
-                int temp = array[j+1];
-                array[j+1] = array[j];
+                int temp = array[j + 1];
+                array[j + 1] = array[j];
                 array[j] = temp;
                 flag = 1;
-            }  
+            }
         }
+
         if (flag == 0)
             break;
     }
@@ -34,12 +62,9 @@ int main()
         cout << "Enter elemnt for position array[" << i << "] = ";
         cin >> array[i];
     }
-    cout<<"The shorted array is: "<<endl;
     bubbleShort(array, size);
-    for (int i = 0; i < size; i++)
-    {
-        cout<<array[i]<<" ";
-    }
+    cout << "The shorted array is: " << endl;
+    display(array, size);
 
     return 0;
 }
--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -35,10 +35,6 @@ void InsertionSort(int array[], int size)
         int key = array[i];
         int j = i - 1;
 
-        // Move elements of arr[0..i-1],
-        // that are greater than key,
-        // to one position ahead of their
-        // current position
         while (j >= 0 && array[j] > key)
         {
             array[j + 1] = array[j];
