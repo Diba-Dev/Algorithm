ff67fdcba1784c3290885a7383fbe3313b186ed8
Diba-Dev <dibadev909090@gmail.com>
2024-06-01 15:19:14 +0600

    insertion short edited

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,25 +4,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void display(int array[],int size);
+void display(int array[], int size);
 void InsertionSort(int array[], int size)
 {
 
     for (int i = 1; i < size; i++)
     {
-        cout<<"pass["<<i<<"] : "<<endl;
-        display(array,size);
-
         int key = array[i];
         int j = i - 1;
-
+        cout << "pass[" << i << "] : " << endl;
         while (j >= 0 && array[j] > key)
         {
             array[j + 1] = array[j];
+            display(array, size);
             j--;
         }
-        array[j + 1] = key;        
-    
+        array[j + 1] = key;
+        display(array, size);
     }
 }
 
@@ -37,30 +35,38 @@ int main()
         cout << "Enter elemnt for position array[" << i << "] = ";
         cin >> array[i];
     }
-    cout << "The shorted array is: " << endl;
+    cout << "The Provided array is: " << endl;
+    display(array, size);
+
     InsertionSort(array, size);
-    cout<<endl<<"The shorted array after pass["<<size<<"] is: "<<endl;
-    display(array,size);
+    cout << endl
+         << "The shorted array after pass[" << size << "] is: " << endl;
+    cout << "The shorted array is: " << endl;
+    display(array, size);
 
     return 0;
 }
 
-void display(int array[],int size){
+void display(int array[], int size)
+{
     int boxWidth = 4;
 
-    for (int i = 0; i < size; ++i) {
-        cout << "+----"; 
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "+----";
     }
     cout << "+" << endl;
 
-    for (int i = 0; i < size; ++i) {
-        cout << "| " <<setw(boxWidth - 2)<< array[i] << " ";
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "| " << setw(boxWidth - 2) << array[i] << " ";
     }
     cout << "|" << endl;
 
     // Print the bottom border
-    for (int i = 0; i < size; ++i) {
-        cout << "+----";  // Adjust the number of '-' according to boxWidth
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "+----"; // Adjust the number of '-' according to boxWidth
     }
     cout << "+" << endl;
 }
